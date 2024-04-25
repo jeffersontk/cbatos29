adc03bf2a6ba98df9158fc4b54200fdd73ef8b3d
jefferson <jscabral@firjan.com.br>
2024-04-25 01:31:28 -0300

    atualizando astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import { sanityIntegration } from "@sanity/astro";
 import react from "@astrojs/react";
 
+import sanity from "@sanity/astro";
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   prefetch: true,
-  integrations: [tailwind(), 
-  sanityIntegration({
-    projectId: "l9mtpnzh",
-    dataset: "production",
-    apiVersion: '2024-04-22',
-    useCdn: false,
-    // Access the Studio on your.url/admin
-    studioBasePath: "/admin"
-  }), react()],
   adapter: vercel(),
+  integrations: [
+    tailwind(), 
+    sanity({
+      projectId: "l9mtpnzh",
+      dataset: "production",
+      apiVersion: '2024-04-22',
+      useCdn: false,
+      // Access the Studio on your.url/admin
+      studioBasePath: "/admin"
+    }), 
+    react()],
 });
