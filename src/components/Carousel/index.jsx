71adff16c960e1fd10f8662187cc592b6b62c3bf
jefferson <jscabral@firjan.com.br>
2024-05-09 00:50:05 -0300

    pagina de home mobile

---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export const Carousel = () => {
+    const [currentImage, setCurrentImage] = useState(0);
+
+    const images = [
+        "/public/slider/infinitamente-mais.jpeg",
+        "/public/slider/vem-pro-culto.jpg",
+        "/public/slider/ebd.jpg",
+    ];
+
+    const nextImage = () => {
+        setCurrentImage((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+    };
+
+    const prevImage = () => {
+        setCurrentImage((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            nextImage();
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, [currentImage]);
+
+    return (
+        <div
+            className="relative"
+        >
+   
+            <div className="overflow-hidden w-full max-h-[488px] md:h-[488px]">
+                <div className="flex max-h-[488px]" style={{ transform: `translateX(-${currentImage * 100}%)`, transition: "transform 0.5s ease" }}>
+                    {images.map((image, index) => (
+                        <img
+                            key={index}
+                            src={image}
+                            alt={`Imagem ${index + 1}`}
+                            className="image w-full"
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-4">
+                {images.map((_, index) => (
+                    <button
+                        key={index}
+                        onClick={() => setCurrentImage(index)}
+                        className={`w-4 h-4 rounded-full ${
+                            index === currentImage ? "bg-gray-500 w-8 transform transition-all duration-500" : "bg-gray-700 opacity-50"
+                        } focus:outline-none`}
+                    ></button>
+                ))}
+            </div>
+            <button
+                onClick={prevImage}
+                className="absolute top-1/2 left-4 transform -translate-y-1/2 h-full px-3 py-1 rounded-md text-white text-sm focus:outline-none"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10 opacity-75 ">
+                <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
+                </svg>
+
+            </button>
+            <button
+                onClick={nextImage}
+                className="absolute top-1/2 right-4 transform -translate-y-1/2 h-full px-3 py-1 rounded-md text-white text-sm focus:outline-none"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10 opacity-75 ">
+                <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
+                </svg>
+
+            </button>
+        </div>
+    );
+};
