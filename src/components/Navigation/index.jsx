f021045f5381285433d8eed8fdeb0740170609ac
jefferson <jscabral@firjan.com.br>
2024-06-11 23:53:57 -0300

    fiz navbar

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react"
+import { useState } from "react";
 
 export const Navigation = ({ pathname }) => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className="relative">
-      <div className="max-w-7xl mx-auto pr-8 sm:px-6 lg:px-8">
+    <div className="relative w-full">
+      <div className="w-full flex justify-end pr-8">
         <div className="flex justify-between items-center py-4 md:justify-start md:space-x-10">
           <div className="-mr-2 -my-2 lg:hidden">
             <button
@@ -30,7 +30,7 @@ export const Navigation = ({ pathname }) => {
               </svg>
             </button>
           </div>
-          <nav className="hidden lg:flex space-x-10 ">
+          <nav className="hidden lg:flex space-x-10">
             <a
               href="/"
               className="text-base font-medium text-white hover:text-gray-300"
@@ -65,86 +65,87 @@ export const Navigation = ({ pathname }) => {
               href="/history"
               className="text-base font-medium text-white hover:text-gray-300"
             >
-              Nossa Historia
+              Nossa História
             </a>
           </nav>
         </div>
       </div>
-
-      <div
-        className={`${
-          isOpen
-            ? "absolute z-50 bg-primary-dark top-0 right-0 h-[100vh] w-[200px] px-4 py-2 transform translate-all duration-500"
-            : "hidden"
-        } md:hidden`}
-      >
-        <header className="flex justify-end">
-          <button onClick={toggleMenu}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-12 h-12 text-gray-50"
+      <div className="absolute right-0 top-0 w-full">
+        <div
+          className={`${
+            isOpen
+              ? "bg-primary-dark h-screen w-full px-4 py-2 transform translate-all duration-500"
+              : "hidden"
+          } md:hidden`}
+        >
+          <header className="flex justify-end">
+            <button onClick={toggleMenu}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-12 h-12 text-gray-50"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </header>
+          <div className="flex flex-col gap-4 mt-4">
+            <a
+              href="/"
+              className={`block text-base font-medium text-white hover:text-gray-300 ${
+                pathname === "/" ? "border-b-2 border-white" : ""
+              }`}
             >
-              <path
-                fillRule="evenodd"
-                d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-        </header>
-        <div className="flex flex-col gap-4 mt-4">
-          <a
-            href="/"
-            className={`block text-base font-medium text-white hover:text-gray-300 ${
-              pathname === "/" ? "border-b-2 border-white" : ""
-            }`}
-          >
-            Início
-          </a>
-          <a
-            href="/ebd"
-            className={`block text-base font-medium text-white hover:text-gray-300 ${
-              String(pathname).includes("/ebd") ? "border-b-2 border-white" : ""
-            }`}
-          >
-            EBD
-          </a>
-          <a
-            href="/cells"
-            className={`block text-base font-medium text-white hover:text-gray-300 ${
-              pathname === "/celula" ? "border-b-2 border-white" : ""
-            }`}
-          >
-            Células
-          </a>
-          <a
-            href="/event"
-            className={`block text-base font-medium text-white hover:text-gray-300 ${
-              pathname === "/event" ? "border-b-2 border-white" : ""
-            }`}
-          >
-            Eventos
-          </a>
-          <a
-            href="/pastoral-team"
-            className={`block text-base font-medium text-white hover:text-gray-300 ${
-              pathname === "/pastoral-team" ? "border-b-2 border-white" : ""
-            }`}
-          >
-            Equipe Pastoral
-          </a>
-          <a
-            href="/history"
-            className={`block text-base font-medium text-white hover:text-gray-300 ${
-              pathname === "/history" ? "border-b-2 border-white" : ""
-            }`}
-          >
-            Nossa Historia
-          </a>
+              Início
+            </a>
+            <a
+              href="/ebd"
+              className={`block text-base font-medium text-white hover:text-gray-300 ${
+                String(pathname).includes("/ebd") ? "border-b-2 border-white" : ""
+              }`}
+            >
+              EBD
+            </a>
+            <a
+              href="/cells"
+              className={`block text-base font-medium text-white hover:text-gray-300 ${
+                pathname === "/cells" ? "border-b-2 border-white" : ""
+              }`}
+            >
+              Células
+            </a>
+            <a
+              href="/event"
+              className={`block text-base font-medium text-white hover:text-gray-300 ${
+                pathname === "/event" ? "border-b-2 border-white" : ""
+              }`}
+            >
+              Eventos
+            </a>
+            <a
+              href="/pastoral-team"
+              className={`block text-base font-medium text-white hover:text-gray-300 ${
+                pathname === "/pastoral-team" ? "border-b-2 border-white" : ""
+              }`}
+            >
+              Equipe Pastoral
+            </a>
+            <a
+              href="/history"
+              className={`block text-base font-medium text-white hover:text-gray-300 ${
+                pathname === "/history" ? "border-b-2 border-white" : ""
+              }`}
+            >
+              Nossa História
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
